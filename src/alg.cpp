43659b7ba5f1482d204c5242fd2b1cf7009cca19
Forzzi3 <125073237+Forzzi3@users.noreply.github.com>
2023-03-11 20:03:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left = 0, right = size - 1, count = 0;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (value < arr[mid]) {
+            right = mid - 1;
+        } else if (value > arr[mid]) {
+            left = mid + 1;
+        } else {
+            count++;
+            int i = 1;
+            while (arr[i] == arr[mid - i]) {
+                count++;
+                i++;
+            }
+            i = 1;
+            while (arr[i] == arr[mid + i]) {
+                count++;
+                i++;
+            }
+            return count;
+        }
+    }
+    return 0;
 }
